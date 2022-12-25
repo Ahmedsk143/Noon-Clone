03fa18576dfabcdcf0cc459b3fc0caca3abfbc3b
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2022-12-25 15:59:34 +0200

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import indexRoute from './routes/indexRoutes';
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -7,6 +8,7 @@ const app: express.Application = express();
 const port = process.env.PORT || 5555;
 
 // Middelwares
+app.use(cors());
 app.use(express.json());
 app.use(
     express.urlencoded({
