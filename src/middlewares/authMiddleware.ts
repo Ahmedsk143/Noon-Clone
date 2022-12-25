37068d7095794412b6e9740dc18638df72ccd87e
Ahmedsk143 <ahmedsayedkhalil143@gmail.com>
2022-12-25 17:42:58 +0200

    token error

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,17 +3,21 @@ import * as jwt from 'jsonwebtoken';
 import { User } from '../models/userModel';
 const AuthMiddleware = (
     req: Request,
-    _res: Response,
+    res: Response,
     next: NextFunction
 ): void => {
     if (req.headers.authorization) {
-        const token = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(
-            token,
-            process.env.PRIVATE_KEY as string
-        ) as User;
-        req.userId = '' + decoded.id;
-        req.email = decoded.email;
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(
+                token,
+                process.env.PRIVATE_KEY as string
+            ) as User;
+            req.userId = '' + decoded.id;
+            req.email = decoded.email;
+        } catch (e) {
+            res.json({ error: 'Invalid token' });
+        }
     }
     next();
 };
